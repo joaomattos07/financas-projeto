89466fc9aac6efc71a96e6bce66f18e75e544408
João Pedro <joaopedro261206@gmail.com>
2026-08-25 21:13:41 -0300

    Update responsividade

--- js/script.js
+++ js/script.js
@@ -4,3 +4,11 @@ const navLinks = document.querySelector('.nav-links');
 navToggle.addEventListener('click', () => {
   navLinks.classList.toggle('is-open');
 });
+
+const navLinkItems = document.querySelectorAll('.nav-links a');
+
+navLinkItems.forEach((link) => {
+  link.addEventListener('click', () => {
+    navLinks.classList.remove('is-open');
+  });
+});
